1f1e7ba34ba478b57445f10a7ce3ad1dddd46b1f
HwangToeMat <46777602+HwangToeMat@users.noreply.github.com>
2020-03-04 18:12:11 +0900

    Downloading OpenImages categorically.
    Helper library for downloading OpenImages(https://storage.googleapis.com/openimages/web/index.html) categorically.

--- main.py
+++ main.py
@@ -16,17 +16,19 @@ parser.add_argument('--set', default="sum", type=str,
                     help="If you want 'sum of sets' : 'sum' else if you want 'intersection' : 'inter'")
 parser.add_argument("--ndata", default=-1, type=int,
                     help="number of data you want")
-parser.add_argument("--label", default="https://storage.googleapis.com/openimages/v5/class-descriptions-boxable.csv", type=str,
+parser.add_argument("--label", default="class-descriptions-boxable.csv", type=str,
                     help="path of 'class-descriptions-boxable.csv'")
-parser.add_argument("--annotation", default="https://storage.googleapis.com/openimages/v6/oidv6-train-annotations-bbox.csv", type=str,
+parser.add_argument("--annotation", default="oidv6-train-annotations-bbox.csv", type=str,
                     help="path of 'xxx-annotations-bbox.csv'")
-parser.add_argument("--imageURL", default="https://storage.googleapis.com/openimages/2018_04/train/train-images-boxable-with-rotation.csv", type=str,
+parser.add_argument("--imageURL", default="train-images-boxable-with-rotation.csv", type=str,
                     help="path of imageURL file.(ex : 'xxx/train-images-boxable-with-rotation.csv')")
 parser.add_argument("--datapath", default="train_data", type=str)
-opt = parser.parse_args()
 
 
-def download(category=opt.category, set=opt.set, ndata=opt.ndata, label=opt.label, annotation=opt.annotation, imageURL=opt.imageURL, datapath=opt.datapath):
+def main():
+    opt = parser.parse_args()
+    print(opt)
+    print('===>> Download or load the csv')
 
     # Download or load the LabelName of Category
     kwargs = {'header': None, 'names': ['LabelName', 'Category']}
@@ -38,7 +40,12 @@ def download(category=opt.category, set=opt.set, ndata=opt.ndata, label=opt.labe
     # Download or load the imageURL
     imageURL = pd.read_csv(opt.imageURL)
 
+    # Preprocess the data
+    print(f'===>> Category : {opt.category}, Type : {opt.set}')
     if opt.set == "inter":
+        if opt.category == []:
+            print('===>> Please enter a category')
+            return False
         Empty_data = True
         for ct in opt.category:
             label_map = dict(label.set_index('Category').loc[[ct], 'LabelName'].to_frame(
@@ -56,16 +63,20 @@ def download(category=opt.category, set=opt.set, ndata=opt.ndata, label=opt.labe
         URL_data.iloc[:opt.ndata, :].dropna(
             subset=['OriginalURL']).loc[:, 'OriginalURL']
     else:
-        label_map = dict(label.set_index('Category').loc[opt.category, 'LabelName'].to_frame(
-        ).reset_index().set_index('LabelName')['Category'])
+        if opt.category == []:
+            label_map = dict(label.set_index('Category').loc[:, 'LabelName'].to_frame(
+            ).reset_index().set_index('LabelName')['Category'])
+        else:
+            label_map = dict(label.set_index('Category').loc[opt.category, 'LabelName'].to_frame(
+            ).reset_index().set_index('LabelName')['Category'])
         label_values = set(label_map.keys())
         Total_data = annotation[annotation.LabelName.isin(label_values)]
         URL_data = Total_data.set_index('ImageID').join(imageURL.set_index(
             'ImageID')).drop_duplicates(['OriginalURL']).iloc[:opt.ndata, :].loc[:, 'OriginalURL']
 
+    # Print remaining_todo
     remaining_todo = len(URL_data) if checkpoints.results is None else\
         len(URL_data) - len(checkpoints.results)
-
     print(f"Parsing {remaining_todo} images "
           f"({len(URL_data) - remaining_todo} have already been downloaded)")
 
@@ -76,16 +87,18 @@ def download(category=opt.category, set=opt.set, ndata=opt.ndata, label=opt.labe
         progress_bar.close()
 
     # Write the images to files, adding them to the package as we go along.
-    if not os.path.isdir(f"{datapath}/"):
-        os.mkdir(f"{datapath}/")
-
+    if not os.path.isdir(f"{opt.datapath}/"):
+        os.mkdir(f"{opt.datapath}/")
     for ((_, r), (_, url)) in zip(Request_data.iteritems(), URL_data.iteritems()):
         image_name = url.split("/")[-1]
-        _write_image(r, image_name, datapath)
+        _write_image(r, image_name, opt.datapath)
+    print("===>> Save the images to files")
 
-    if not os.path.isdir(f"{datapath}/bbox/"):
-        os.mkdir(f"{datapath}/bbox/")
-    Total_data.to_csv(f"{datapath}/bbox/bbox_data.csv")
+    # Write the bbox data to csv file.
+    if not os.path.isdir(f"{opt.datapath}/bbox/"):
+        os.mkdir(f"{opt.datapath}/bbox/")
+    Total_data.to_csv(f"{opt.datapath}/bbox/bbox_data.csv")
+    print("===>> Save the bbox data to csv file")
 
 
 @ratelim.patient(5, 5)
@@ -112,5 +125,4 @@ def _write_image(r, image_name, datapath):
 
 
 if __name__ == '__main__':
-    download(category=opt.category, set=opt.set, ndata=opt.ndata, label=opt.label,
-             annotation=opt.annotation, imageURL=opt.imageURL, datapath=opt.datapath)
+    main()
